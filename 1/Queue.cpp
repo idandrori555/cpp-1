7c2bacead511186d92f7d3121b2540025a5c8f43
idan <idandrori555@gmail.com>
2025-10-27 19:58:47 +0200

    added cpp files

---
+++ 1/Queue.cpp
@@ -0,0 +1,31 @@
+#include "Queue.h"
+#include <cstring>
+
+void initQueue(Queue *q, unsigned int size)
+{
+}
+
+void cleanQueue(Queue *q)
+{
+}
+
+void enqueue(Queue *q, unsigned int newValue)
+{
+}
+
+int dequeue(Queue *q)
+{
+}
+
+bool isEmpty(Queue *s)
+{
+}
+
+bool isFull(Queue *s)
+{
+}
+
+int main(void)
+{
+  return 0;
+}
